5de0254513d17fc8dbc4b1f450184cd9eeb98b7f
Tu Ninh <tuninh.2411@gmail.com>
2026-02-22 19:58:49 +0000

    Update H code

--- Codeforces/Div4Round1074/H.rs
+++ Codeforces/Div4Round1074/H.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 use std::io::{ self, BufRead };
 use std::str::FromStr;
+use std::cmp::min;
 
 struct Scanner<R> {
     reader: R,
@@ -61,58 +62,39 @@ impl Solution {
         Self {
             n: n,
             k: sc.next(),
-            a: std::iter::once(0).chain(sc.next_values(n)).collect(),
+            a: sc.next_values(n),
         }
     }
 
-    fn find_bad_position_bound(
-        &self,
-        psum: &[i64],
-        skip_index: usize,
-        l: usize,
-        r: usize
-    ) -> usize {
-        let val: i64 = self.a[skip_index];
-        let mut l: usize = l;
-        let mut r: usize = r;
-        let mut ans: usize = r + 1;
-        while l <= r {
-            let mid: usize = (l + r) >> 1;
-            let is_bad: bool = if mid > skip_index {
-                psum[mid] < val * 2
-            } else {
-                if mid - 1 == 0 { false } else { psum[mid - 1] < val }
-            };
-            if is_bad {
-                ans = mid;
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
+    fn range_sum_ignoring_index(&self, psum: &[i64], l: usize, r: usize, index: usize) -> i64 {
+        let mut ans: i64 = 0;
+        if r < l {
+            return ans;
         }
-        return ans;
+        if r < index {
+            ans = psum[r] - (if l == 0 { 0_i64 } else { psum[l - 1] });
+            return ans;
+        }
+        if l > index {
+            ans = psum[r + 1] - psum[l];
+        } else {
+            ans = psum[r + 1] - (if l == 0 { 0_i64 } else { psum[l - 1] }) - self.a[index];
+        }
+        ans
     }
 
-    fn find_cheating_limit_bound(
-        &self,
-        bad_positions: &[i64],
-        add: i64,
-        l: usize,
-        r: usize
-    ) -> usize {
-        let mut l: usize = l;
-        let mut r: usize = r;
-        let mut ans = r + 1;
+    fn upper_bound_ignoring_index(&self, psum: &[i64], target: i64, index: usize) -> usize {
+        let mut l: usize = 0;
+        let mut r: usize = self.n - 1;
         while l <= r {
             let mid: usize = (l + r) >> 1;
-            if bad_positions[mid] + add > self.k {
+            if mid == 0 || self.range_sum_ignoring_index(&psum, 0, mid - 1, index) < target {
                 l = mid + 1;
             } else {
-                ans = mid;
                 r = mid - 1;
             }
         }
-        return ans;
+        r
     }
 
     fn solve(&self) {
@@ -123,49 +105,48 @@ impl Solution {
                 Some(*s)
             })
             .collect();
-        let mut count_bad_positions: Vec<i64> = vec![0; self.n + 1];
-        for i in (1..self.n + 1).rev() {
-            if i > 1 && self.a[i] > psum[i - 1] {
-                count_bad_positions[i] += 1;
-            }
-            if i + 1 <= self.n {
-                count_bad_positions[i] += count_bad_positions[i + 1];
-            }
-        }
-        println!("{:?}", psum);
-        println!("{:?}", count_bad_positions);
-
-        for i in 1..self.n + 1 {
+        for i in 0..self.n {
             let mut ans: i64 = 0;
-            let rhs_bad_ub: usize = self.find_bad_position_bound(&psum, i, i + 1, self.n);
-            let rhs_bad_lb: usize = self.find_cheating_limit_bound(
-                &count_bad_positions,
-                0,
-                i + 1,
-                self.n
-            );
-            let lhs_bad_ub: usize = self.find_bad_position_bound(&psum, i, 1, i - 1);
-            let add: i64 = if i > 1 && self.a[i] > psum[i - 1] { -1_i64 } else { 0_i64 };
-            let lhs_bad_lb: usize = self.find_cheating_limit_bound(
-                &count_bad_positions,
-                add,
-                1,
-                i - 1
-            );
-            if rhs_bad_ub <= self.n && rhs_bad_lb <= rhs_bad_ub {
-                ans += (rhs_bad_ub - rhs_bad_lb + 1) as i64;
+            let last_starting_pos: usize = self.upper_bound_ignoring_index(&psum, self.a[i], i);
+            let mut bad_positions: Vec<usize> = Vec::new();
+            let mut current_psum: i64 = 0;
+            loop {
+                let v: i64 = current_psum + self.a[i];
+                let bad_pos: usize = self.upper_bound_ignoring_index(
+                    &psum,
+                    v * 2 + 1 - self.a[i],
+                    i
+                );
+                if bad_pos >= self.n - 1 {
+                    break;
+                }
+                let psum_before_bad_v: i64 =
+                    (if bad_pos >= 1 {
+                        self.range_sum_ignoring_index(&psum, 0, bad_pos - 1, i)
+                    } else {
+                        0_i64
+                    }) + self.a[i];
+                if self.range_sum_ignoring_index(&psum, bad_pos, bad_pos, i) > psum_before_bad_v {
+                    bad_positions.push(bad_pos + 1);
+                }
+                current_psum =
+                    psum_before_bad_v +
+                    self.range_sum_ignoring_index(&psum, bad_pos, bad_pos, i) -
+                    self.a[i];
             }
-            if lhs_bad_ub <= i - 1 && lhs_bad_lb <= lhs_bad_ub {
-                ans += (lhs_bad_ub - lhs_bad_lb + 1) as i64;
+            if self.k == 0 {
+                if bad_positions.len() == 0 {
+                    ans += (last_starting_pos as i64) + 1;
+                }
+            } else if bad_positions.len() > (self.k as usize) {
+                ans += (self.n - bad_positions[bad_positions.len() - (self.k as usize)]) as i64;
+            } else if bad_positions.len() < (self.k as usize) {
+                ans += self.n as i64;
+            } else {
+                let temp = min(self.n, last_starting_pos + 1 + self.n - bad_positions[0]);
+                ans += temp as i64;
             }
-            if
-                (self.a[i] > psum[i - 1] && count_bad_positions[i] <= self.k) ||
-                (self.a[i] <= psum[i - 1] && count_bad_positions[i] < self.k)
-            {
-                ans += 1;
-            }
-            println!("{} {} {} {} {}", rhs_bad_lb, rhs_bad_ub, lhs_bad_lb, lhs_bad_ub, add);
-            println!("{} ", ans);
+            print!("{} ", ans);
         }
         println!("");
     }
