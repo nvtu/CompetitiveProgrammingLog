9054b35289cab5ba10bba0623ec69fd4d8bb24e9
Tu Ninh <tuninh.2411@gmail.com>
2026-02-24 22:54:53 +0000

    Update codes for round 1076

--- Codeforces/Div3Round1076/F.rs
+++ Codeforces/Div3Round1076/F.rs
@@ -1,6 +1,7 @@
+#![allow(dead_code)]
 use std::io::{ self, BufRead };
 use std::str::FromStr;
-use std::cmp::max;
+use std::cmp::{ min, max };
 
 struct Scanner<R> {
     reader: R,
@@ -51,41 +52,52 @@ impl<R: BufRead> Scanner<R> {
 
 fn solve<R: BufRead>(sc: &mut Scanner<R>) {
     let n: usize = sc.next();
-    let mut a: Vec<i64> = sc.next_values(n);
-    let b: Vec<i64> = sc.next_values(n);
-    a.sort_unstable();
-    a.push(0);
-    let mut cnt: i64 = 1;
-    let mut a_with_cnt: Vec<(i64, i64)> = Vec::new();
+    let a: (i64, i64) = (sc.next(), sc.next());
+    let b: (i64, i64) = (sc.next(), sc.next());
+    let mut p: Vec<(i64, i64)> = vec![(0, 0); n];
+    for i in 0..n {
+        p[i].0 = sc.next();
+    }
+    for i in 0..n {
+        p[i].1 = sc.next();
+    }
+    p.sort_unstable();
+    p.push((0, 0));
+    let mut mmin: i64 = i64::MAX;
+    let mut mmax: i64 = 0;
+    let mut pizza: Vec<Vec<i64>> = Vec::new();
+    let mut ans: i64 = p[0].0 - a.0 + b.0 - p[n - 1].0;
+
+    pizza.push([a.1, a.1].to_vec());
     for i in 1..n + 1 {
-        if a[i] == a[i - 1] {
-            cnt += 1;
-        } else {
-            a_with_cnt.push((a[i - 1], cnt));
-            cnt = 1;
+        mmin = min(mmin, p[i - 1].1);
+        mmax = max(mmax, p[i - 1].1);
+        if p[i].0 != p[i - 1].0 {
+            pizza.push([mmin, mmax].to_vec());
+            mmin = i64::MAX;
+            mmax = 0;
+            if i < n {
+                ans += p[i].0 - p[i - 1].0;
+            }
         }
     }
-    let b_pcount: Vec<i64> = b
-        .iter()
-        .scan(0, |s, x| {
-            *s += x;
-            Some(*s)
-        })
-        .collect();
-    let mut agg_count: Vec<i64> = vec![0; a_with_cnt.len() + 1];
-    for i in (0..a_with_cnt.len()).rev() {
-        agg_count[i] = agg_count[i + 1] + a_with_cnt[i].1;
-    }
-    let mut ans: i64 = 0;
-    let mut r: usize = b_pcount.len();
-    for i in 0..a_with_cnt.len() {
-        let count: i64 = agg_count[i];
-        while r > 0 && b_pcount[r - 1] > count {
-            r -= 1;
+    pizza.push([b.1, b.1].to_vec());
+    let m: usize = pizza.len();
+    let mut f: Vec<Vec<i64>> = vec![vec![i64::MAX; 2]; m];
+    f[0][0] = 0;
+    f[0][1] = 0;
+    for i in 1..m {
+        for j in 0..2 {
+            f[i][j] = min(
+                f[i][j],
+                min(
+                    f[i - 1][j] + (pizza[i - 1][j] - pizza[i][1 - j]).abs(),
+                    f[i - 1][1 - j] + (pizza[i - 1][1 - j] - pizza[i][1 - j]).abs()
+                ) + (pizza[i][j] - pizza[i][1 - j]).abs()
+            );
         }
-        let score: i64 = a_with_cnt[i].0 * (r as i64);
-        ans = max(ans, score);
     }
+    ans += f[m - 1][0];
     println!("{}", ans);
 }
 
